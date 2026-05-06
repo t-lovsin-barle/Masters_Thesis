6fda49ac69dca24778edd88b2ba8807df282d700
t-lovsin-barle <tristan.lovsin@gmail.com>
2026-05-06 17:08:40 +0200

    Update cocentric_circles.py

--- experiments/cocentric_circles.py
+++ experiments/cocentric_circles.py
@@ -123,5 +123,5 @@ for noise in noise_levels:
                     height=400,
                     )
 
-                filename = FIG_DIR_ / f"circles_{filter}_{clusterer}_{overlap_frac}.svg"
+                filename = FIG_DIR_ / f"circles_{filter_func}_{clusterer}_{overlap_frac}.svg"
                 fig.write_image(str(filename))
